75cbaa0957d4e3b1f9270bec7b03b089924183d1
Elina Razina <elina.razina@dataart.com>
2019-08-27 19:42:27 +0300

    layout of videolist

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,16 +1,42 @@
 import React from 'react';
 import VideoListItem from './video_list_item';
 
-const VideoList = (props) => {
-    const videoItems = props.videos.map((item) => {
-        return <VideoListItem video={item} key={item}>{item}</VideoListItem>
-    })
-       return(
+class VideoList extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            videos: []
+        }
+
+    }
+
+
+    render(){
+        const videoList = this.props.video.map((item) => {
+            // console.log(item.snippet.thumbnails.default.url)
+            return <VideoListItem key={item.etag} video={item}/>
+        });
+
+        return(
             <div>
-               {videoItems} 
+                <ul className="col-md-4 list-group">
+                    {videoList}
+                </ul>
             </div>
         )
-
+    }
 }
+
+// const VideoList = (props) => {
+//     const videoItems = props.videos.map((item) => {
+//         return <VideoListItem video={item} key={item.etag}>{item}</VideoListItem>
+//     })
+//        return(
+//             <div>
+//                {videoItems} 
+//             </div>
+//         )
+
+// }
 
 export default VideoList;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
-import  {API_KEY} from './constants/constants';
+import  { API_KEY } from './constants/constants';
 
 const api_key = API_KEY;
 
@@ -11,24 +11,23 @@ class App extends Component{
 
   constructor(props){
     super(props);
-
     this.state = {
       videos: []
-    }
-
-    YTSearch({key: api_key, value: 'surfboards'}, (data) => {
-      this.setState({videos: data});
+    };
+    YTSearch({ key: api_key, term: 'surfboards'}, (videos) => {
+      this.setState({videos});
+      console.log(this.state.videos)
+      // this.setState(videos:videos)
     });
-
-
   }
+
   render(){
     return(
       <div>
         <SearchBar />
-        <VideoList videos={this.state.videos}/>
+        <VideoList video={this.state.videos}/>
       </div>
-    );
+    )
   }
 }
 
